69ad3536ade441d17e91191ef54b22d3fa9dcba7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-07-12 16:17:02 -0700

    Add 'build-logic' folder for managing build
    Signed-off-by: Joel Van Eenwyk <joel.vaneenwyk@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,4 +5,15 @@
  */
 
 rootProject.name = "helium"
+
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 include("libs")
