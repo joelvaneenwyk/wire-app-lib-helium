5ba89f3059cbf2eb69524509f6b2aabdb2fee9b7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-07-13 20:20:50 -0700

    Upgrade to latest version of 'com.wire.xenon' (#20)
    * Update package 'com.wire.helium' to '1.3.2'
    * Add 'build-logic' folder for managing build
    * Disable 'Picture' class usage temporarily
    * Update 'xenon' to latest

--- src/test/java/com/wire/helium/ApplicationTest.java
+++ src/test/java/com/wire/helium/ApplicationTest.java
@@ -4,20 +4,17 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.wire.xenon.MessageHandlerBase;
 import com.wire.xenon.WireClient;
 import com.wire.xenon.assets.MessageText;
-import com.wire.xenon.assets.Picture;
 import com.wire.xenon.backend.models.Conversation;
 import com.wire.xenon.backend.models.User;
 import com.wire.xenon.crypto.CryptoDatabase;
 import com.wire.xenon.crypto.storage.JdbiStorage;
 import com.wire.xenon.factories.CryptoFactory;
 import com.wire.xenon.factories.StorageFactory;
-import com.wire.xenon.models.AssetKey;
 import com.wire.xenon.models.PhotoPreviewMessage;
 import com.wire.xenon.models.RemoteMessage;
 import com.wire.xenon.models.TextMessage;
 import com.wire.xenon.state.JdbiState;
 import com.wire.xenon.tools.Logger;
-import com.wire.xenon.tools.Util;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +22,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import java.util.Collections;
 import java.util.UUID;
+
+// @joelvaneenwyk Re-enable after determining alternative to 'Picture'
+// import com.wire.xenon.assets.Picture;
+// import com.wire.xenon.models.AssetKey;
+// import com.wire.xenon.tools.Util;
 
 @Disabled("This is an integration test which requires access to Wire account.")
 public class ApplicationTest extends DatabaseTestBase {
@@ -88,13 +90,14 @@ public class ApplicationTest extends DatabaseTestBase {
         // Send text
         wireClient.send(new MessageText("Hi there!"));
 
+        // @joelvaneenwyk Re-enable after determining alternative to 'Picture'
         // Send Image
-        final byte[] bytes = Util.getResource("moon.jpg");
-        Picture image = new Picture(bytes, "image/jpeg");
-        AssetKey assetKey = wireClient.uploadAsset(image);
-        image.setAssetKey(assetKey.id);
-        image.setAssetToken(assetKey.token);
-        wireClient.send(image);
+        //final byte[] bytes = Util.getResource("moon.jpg");
+        //Picture image = new Picture(bytes, "image/jpeg");
+        //AssetKey assetKey = wireClient.uploadAsset(image);
+        //image.setAssetKey(assetKey.id);
+        //image.setAssetToken(assetKey.token);
+        //wireClient.send(image);
 
         Thread.sleep(15 * 1000);
         app.stop();
